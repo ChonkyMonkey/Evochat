a152a231423514297c4f879e72b72532dfaca68c
ChonkyMonkey <toniquaxe@gmail.com>
2025-06-15 20:45:20 +0200

    trying to fix environment issue

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -3,6 +3,8 @@
 interface ImportMetaEnv {
   readonly VITE_ENABLE_LOGGER: string;
   readonly VITE_LOGGER_FILTER: string;
+  readonly VITE_PADDLE_CLIENT_TOKEN: string;
+  readonly VITE_PADDLE_ENVIRONMENT: string;
   // Add other env variables here
 }
 
